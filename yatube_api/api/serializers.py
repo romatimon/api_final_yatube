316a27c938c064a9a4fe1e206d31803545fcde57
Роман Тимонин <rtimonin569@gmail.com>
2023-10-12 01:13:01 +0300

    add first commit

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,11 +1,12 @@
-from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.serializers import (CurrentUserDefault, ModelSerializer,
+                                        ValidationError)
+from rest_framework.validators import UniqueTogetherValidator
+
+from posts.models import Comment, Follow, Group, Post, User
 
 
-from posts.models import Comment, Post
-
-
-class PostSerializer(serializers.ModelSerializer):
+class PostSerializer(ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
@@ -13,11 +14,43 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+class CommentSerializer(ModelSerializer):
+    author = SlugRelatedField(
+        read_only=True,
+        slug_field='username',
     )
 
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post',)
+
+
+class GroupSerializer(ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Group
+
+
+class FollowSerializer(ModelSerializer):
+    user = SlugRelatedField(read_only=True, slug_field='username',
+                            default=CurrentUserDefault())
+    following = SlugRelatedField(
+        queryset=User.objects.all(), slug_field='username')
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following'],
+                message='Вы уже подписаны на этого автора.'
+            )
+        ]
+
+    def validate_following(self, value):
+        if self.context['request'].user == value:
+            raise ValidationError('Нельзя подписаться на самого себя.')
+        return value
